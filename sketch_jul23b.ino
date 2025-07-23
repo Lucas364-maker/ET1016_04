20eac8c3f2837bfee898a5e064c728e5f618aa8b
Lucas364-maker <MYATBHONE.25@ichat.sp.edu.sg>
2025-07-23 12:53:45 +0630

    Add files via upload

---
+++ sketch_jul23b.ino
@@ -0,0 +1,23 @@
+//Name:Myat Bhone Ko
+//Adm:2539052
+//Class:DCEP/FT/1A/04
+
+//Name:Zheng Zhe Ming Jayvis
+//Adm:2517155
+//Class:DCEP/FT/1A/04
+
+//The temperature and humidity senor are used to moniter the plant and depending on it, the led green will be shown, if water is not needed, and, led red will be shown if water is needed.
+
+
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
