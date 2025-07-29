7b7b05521cd71956d84b0ed385fa31c4a5fa889b
Lucas364-maker <MYATBHONE.25@ichat.sp.edu.sg>
2025-07-29 12:22:23 +0630

    Update sketch_jul23b.ino

--- sketch_jul23b.ino
+++ sketch_jul23b.ino
@@ -7,31 +7,32 @@
 //Class:DCEP/FT/1A/04
 
 //The temperature and humidity senor are used to moniter the plant and depending on it, the led green will be shown, if water is not needed, and, led red will be shown if water is needed.
+#define blue 6
+#define blue_B 9
 
 using namespace std;
-
+void Bluebutton(int x);
 
 
 void setup() {
   // put your setup code here, to run once:
-disp.init();  
-	dht.begin();
+	pinMode(blue,OUTPUT);
+	pinMode(blue_B,INPUT);
 }
 
 void loop() {// Reading temperature or humidity takes about 250 milliseconds!
   // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
-  float h = dht.readHumidity();
-  float t = dht.readTemperature();
-
-  // check if returns are valid, if they are NaN (not a number) then something went wrong!
-  if (isnan(t) || isnan(h)) {
-    displayError();
-  } 
-  else{
-    displayTemperature((int8_t)t);//
-    delay(3000);
-    displayHumidity((int8_t)h);//
-    delay(2000);
   
+}
 
+void Bluebutton(int x)
+{
+	
+  if (x == HIGH) {
+    // turn LED on:
+    digitalWrite(blue, HIGH);
+  } else {
+    // turn LED off:
+    digitalWrite(blue, LOW);
+  }
 }
